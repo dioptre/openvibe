528931eda890f9a8fa15c743b42f159ae36ed9ef
Jussi Lindgren <jussi.lindgren@inria.fr>
2017-10-10 14:32:15 +0200

    Build: Fixed Windows dependency installer

--- install_dependencies.cmd
+++ install_dependencies.cmd
@@ -5,6 +5,11 @@ setlocal enableextensions
 set base_dir=%~dp0
 set dependencies_dir=%base_dir%\dependencies
 
+REM As long as the sdk/designer dependencies are hosted within Mensia, 
+REM you need to provide credentials below...
+set PROXYPASS=
+set URL=https://extranet.mensiatech.com/dependencies
+
 :parameter_parse
 if /i "%1"=="-h"  (
 	echo Usage: install_dependencies.cmd [--dependencies-dir directory]
@@ -23,11 +28,15 @@ if /i "%1"=="-h"  (
 	Goto parameter_parse
 )
 
+if not exist "%dependencies_dir%\arch\data" ( mkdir "%dependencies_dir%\arch\data" )
+if not exist "%dependencies_dir%\arch\build\windows" ( mkdir "%dependencies_dir%\arch\build\windows" )
+if not exist "%dependencies_dir%_x64\arch\build\windows" ( mkdir "%dependencies_dir%_x64\arch\build\windows" )
+
 set base_dir=%~dp0
 echo Installing sdk dependencies
 cd %base_dir%\sdk\scripts
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-install-dependencies.ps1" -manifest_file .\windows-dependencies.txt -dest_dir %dependencies_dir% -lazy
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-install-dependencies.ps1" -manifest_file .\tests-data.txt -dest_dir %dependencies_dir% -lazy
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-get-dependencies.ps1" -manifest_file .\windows-dependencies-x86.txt -dest_dir %dependencies_dir%
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-get-dependencies.ps1" -manifest_file .\tests-data.txt -dest_dir %dependencies_dir%
 if !errorlevel! neq 0 (
 	echo Error while installing SDK dependencies
 	exit /b !errorlevel!
@@ -35,16 +44,19 @@ if !errorlevel! neq 0 (
 
 echo Installing Designer dependencies
 cd %base_dir%\designer\scripts
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-install-dependencies.ps1" -manifest_file .\windows-dependencies.txt -dest_dir %dependencies_dir% -lazy
-rem powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-install-dependencies.ps1" -manifest_file .\windows-dependencies-sdk.txt -dest_dir %dependencies_dir% -lazy
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-get-dependencies.ps1" -manifest_file .\windows-dependencies.txt -dest_dir %dependencies_dir%
 if !errorlevel! neq 0 (
 	echo Error while installing Designer dependencies
 	exit /b !errorlevel!
 )
 
+REM The 'extras' dependencies are hosted by Inria ...
+set PROXYPASS=anon:anon
+set URL=http://openvibe.inria.fr/dependencies/win32/2.0.0/
+
 echo Installing OpenViBE extras dependencies
 cd %base_dir%\extras\scripts
-powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-install-dependencies.ps1" -manifest_file .\windows-dependencies.txt -dest_dir %dependencies_dir% -lazy
+powershell.exe -NoProfile -ExecutionPolicy Bypass -file "%base_dir%\sdk\scripts\windows-get-dependencies.ps1" -manifest_file .\windows-dependencies.txt -dest_dir %dependencies_dir%
 if !errorlevel! neq 0 (
 	echo Error while building extras
 	exit /b !errorlevel!
